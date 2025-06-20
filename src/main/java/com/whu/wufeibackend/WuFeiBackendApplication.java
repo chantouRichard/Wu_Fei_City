4373d027e8b5e903d832d9b2da7b7797541b38e9
chantouRichard <2897167455@qq.com>
2025-06-21 00:13:10 +0800

    后端项目初始化——陈涛6.21

---
+++ src/main/java/com/whu/wufeibackend/WuFeiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.whu.wufeibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuFeiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WuFeiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whu/wufeibackend/WuFeiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whu.wufeibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WuFeiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
