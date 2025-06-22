9b3b419892ddea53b2bec2205173ead4a26496d6
Anny <3541749496@qq.com>
2025-06-22 14:43:11 +0800

    :flags: base demo for spring with mybatis

--- SQL/test/select_task_poi.sql
+++
@@ -1,9 +0,0 @@
-SELECT *
-FROM tasks
-WHERE status = 'WAITING'
-    AND (6371 * acos(
-        cos(radians(39.9042)) * cos(radians(latitude)) *
-        cos(radians(longitude) - radians(116.4074)) +
-        sin(radians(39.9042)) * sin(radians(latitude))
-    )) <= 500
-    AND NOW() < deadline;
